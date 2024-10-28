8a13c76cc360eccf5d8d44b6691fd57bcbe483c8
Hamed SYLLA <hamedcedric.sylla@sii.fr>
2024-10-28 16:31:47 +0100

    feat add validation handler

---
+++ src/main/java/com/faniko/api_faniko/rest/config/ValidationControllerAdvice.java
@@ -0,0 +1,32 @@
+package com.faniko.api_faniko.rest.config;
+
+import lombok.NonNull;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ValidationControllerAdvice extends ResponseEntityExceptionHandler {
+
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            @NonNull HttpHeaders headers,
+            @NonNull HttpStatusCode status,
+            @NonNull WebRequest request
+    ) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return new ResponseEntity<>(errors, headers, status);
+    }
+}
